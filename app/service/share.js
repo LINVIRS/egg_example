e1ce8c9493021853ffe04dfeb46202b0c7922781
Administrator <admin@example.com>
2020-10-25 15:31:53 +0800

    查看分享

---
+++ app/service/share.js
@@ -0,0 +1,22 @@
+'use strict'
+const Service = require('egg').Service
+
+class ShareService extends Service {
+  async isExist(sharedurl, options = {}) {
+    console.log('分享地址是: ' + sharedurl)
+    let s = await this.app.model.Share.findOne({
+      where: {
+        sharedurl,
+        iscancel: 0,
+      },
+      ...options,
+    })
+
+    if (!s) {
+      return this.ctx.throw(404, '该分享已失效')
+    }
+    return s
+  }
+}
+
+module.exports = ShareService
