9f7277c17c1911c911531706d6475200b37f1d8c
Administrator <admin@example.com>
2020-10-26 22:20:05 +0800

    完善egg

---
+++ egg/app/router.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = (app) => {
+  const { router, controller } = app
+  router.get('/', controller.home.index)
+  router.get('/list', controller.home.list)
+  // 用户注册
+  router.post('/reg', controller.user.reg)
+  router.post('/login', controller.user.login)
+  // 退出登录
+  router.post('/logout', controller.user.logout)
+  router.post('/upload', controller.file.upload)
+  //剩余容量
+  router.get('/getsize', controller.user.getSize)
+
+  router.get('/file', controller.file.list)
+  //创建文件夹
+  router.post('/file/createdir', controller.file.createdir)
+  //更改名字
+  router.post('/file/rename', controller.file.rename)
+  router.post('/file/delete', controller.file.delete)
+  router.get('/file/search', controller.file.search)
+
+  router.post('/share/create', controller.share.create)
+  router.get('/share/list', controller.share.list)
+  router.get('/share/:sharedurl', controller.share.read)
+  router.post('/share/save_to_self', controller.share.saveToSelf)
+}
---
+++ egg/config/plugin.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type Egg.EggPlugin */
+module.exports = {
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
+  sequelize: {
+    enable: true,
+    package: 'egg-sequelize',
+  },
+  valparams: {
+    enable: true,
+    package: 'egg-valparams',
+  },
+  redis: {
+    enable: true,
+    package: 'egg-redis',
+  },
+  jwt: {
+    enable: true,
+    package: 'egg-jwt',
+  },
+  oss: { enable: true, package: 'egg-oss' },
+}
